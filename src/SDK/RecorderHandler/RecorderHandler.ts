87621bed9cec2a0363ff3a7a7a97fcdd7e4df73d
Albin CR <Albinzr@gmail.com>
2020-04-23 23:57:13 +0530

    updated socket io option only websocket

--- src/SDK/RecorderHandler/RecorderHandler.ts
+++ src/SDK/RecorderHandler/RecorderHandler.ts
@@ -42,7 +42,7 @@ export default class RecorderHandler {
                 this.recorder.start(document.body);
 
                 let io = (window as any).io;
-                this.socket = io.connect(host, {transports:['websocket', 'polling']});
+                this.socket = io.connect(host, {transports:['websocket']});
                 this.socket.once('connect', this.onConnect);
                 this.socket.once('reconnect', this.onConnect);
                 this.socket.once('disconnect', this.onDisconnect);
