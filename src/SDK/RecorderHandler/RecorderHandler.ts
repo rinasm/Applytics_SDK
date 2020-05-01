1b91b343dba7f818da80c8914915be0e76269859
rinas <rinasmusthafa@live.com>
2020-05-01 22:36:07 +0530

    snapshot saperated from events reverted

--- src/SDK/RecorderHandler/RecorderHandler.ts
+++ src/SDK/RecorderHandler/RecorderHandler.ts
@@ -117,7 +117,7 @@ export default class RecorderHandler {
         // if(event && event.type === eventTypes.snapshot && ) {
         //     this.emitToSocket(event.type, event);
         // } else if(event) {
-        //     this.rcDataBuffer.push(event);
+            this.rcDataBuffer.push(event);
         // }
     }
 
