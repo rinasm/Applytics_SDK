7f5ce995350f98c9fd4b0f64f12b7a442c49ca4e
Albin CR <albinzr@gmail.com>
2020-05-04 21:54:46 +0530

    Update RecorderHandler.ts

--- src/SDK/RecorderHandler/RecorderHandler.ts
+++ src/SDK/RecorderHandler/RecorderHandler.ts
@@ -43,7 +43,7 @@ export default class RecorderHandler {
 
                 let io = (window as any).io;
                 this.socket = io.connect(host, {
-                    query: `sid=${this.sid}`,
+                    query: "albin",
                     transports:['websocket'],
                 });
                 this.socket.once('connect', this.onConnect);
