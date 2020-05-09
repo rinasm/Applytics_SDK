7af730d078df55c34583d5fd8598f5ede14dc3e2
Albin CR <Albinzr@gmail.com>
2020-05-09 18:34:23 +0530

    update

--- main.go
+++ main.go
@@ -9,7 +9,7 @@ func main() {
 	fs := http.FileServer(http.Dir("./static"))
 	http.Handle("/", fs)
 
-	log.Println("Listening on :4000...")
+	log.Println("Listening on :4000....")
 	err := http.ListenAndServe(":4000", nil)
 	if err != nil {
 		log.Fatal(err)
