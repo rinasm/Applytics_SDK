c3e57aea7163c5d665594a9e41412a3a7f0c0439
rinas <rinasmusthafa@live.com>
2020-05-25 15:26:37 +0530

    sdk resude code added: bug fix

--- src/SDK/index.ts
+++ src/SDK/index.ts
@@ -3,7 +3,9 @@ import RecorderHandler from './RecorderHandler/RecorderHandler';
 
 const startARC=(clientId:String, appId:String, src:any, arccsrc=false)=> {
     console.log('[ARC] Recorder Handler Initiated, Client ID', clientId, 'App ID', appId, performance.now());
-    localStorage.setItem('arccsrc', src);
+    if(src) {
+        localStorage.setItem('arccsrc', src);
+    }
     new RecorderHandler({clientId, appId, arccsrc});
 }
 
