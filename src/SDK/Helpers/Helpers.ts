63c1d89713a8bf2b92b9535c13cd04583984c848
rinas <rinasmusthafa@live.com>
2020-05-27 17:48:14 +0530

    forced dim [img] added: onload style event

--- src/SDK/Helpers/Helpers.ts
+++ src/SDK/Helpers/Helpers.ts
@@ -21,7 +21,7 @@ export const getSID =()=> {
     try {
         arcsid = JSON.parse(arcsid) as any
     } catch (e) {}
-    if((arcsid && arcsid.sid && (document.referrer || '').indexOf(window.location.host) !== -1) || performance.navigation.type === 1) {
+    if(arcsid && arcsid.sid && ((document.referrer || '').indexOf(window.location.host) !== -1 || performance.navigation.type === 1)) {
         (window as any).ARCNavigation = true;
         (window as any).sidinit = Date.now() - sidinit;
         console.log('[ARC] ' + (performance.navigation.type === 1 ? 'Refresh' : 'Navigation') +' Detected');
