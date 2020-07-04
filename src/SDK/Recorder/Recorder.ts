78e0e9faddeac275abb56500c980fdfdd4241f93
rinas <rinasmusthafa@live.com>
2020-07-04 18:24:12 +0530

    MessageHandler 2.0 testing

--- src/SDK/Recorder/Recorder.ts
+++ src/SDK/Recorder/Recorder.ts
@@ -47,7 +47,7 @@ export default class Recorder {
         this.windowEventHandler = new WindowEventHandler({ getRecorder: ()=> this });
         this.webRequestHandler = new WebRequestHandler({ getRecorder: ()=> this });
         this.metaDataHandler = new MetaDataHandler({ getRecorder: ()=> this });
-        console.log('[ARC] Recorder Initiated. V 0.5.2');
+        console.log('[ARC] Recorder Initiated. V 0.5.3');
     }    
 
     start =(node: any)=> {
