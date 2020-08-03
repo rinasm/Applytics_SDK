77c1f7747a22f7a1764383f21b25c9bbf0ce5de3
rinas <rinasmusthafa@live.com>
2020-08-03 14:30:21 +0530

    ARC Start method updated

--- src/SDK/index.ts
+++ src/SDK/index.ts
@@ -1,5 +1,5 @@
 import RecorderHandler from './RecorderHandler/RecorderHandler';
-
+import {SDK_VERSION} from './Constants/Constants'
 
 const startARC=(clientId:String, appId:String, src:any, arccsrc=false)=> {
     let host = atob((appId||'').split('-')[0]);
@@ -17,6 +17,8 @@ const startARC=(clientId:String, appId:String, src:any, arccsrc=false)=> {
     }
     (window as any).__ARC_DEV__ = __ARC_DEV__;
 
+    if((window as any).__ARC_DEV__) console.log('[ARC] SDK VERSION ', SDK_VERSION)
+
     /**
      *  Validating SDK Host persmission
      */
@@ -25,13 +27,13 @@ const startARC=(clientId:String, appId:String, src:any, arccsrc=false)=> {
         if((window as any).__ARC_DEV__) console.error('[ARC] Invalid host ' + host + '. This host don\'t have permission to run Applytics SDK.');
     } else {
         if((window as any).__ARC_DEV__) console.log('[ARC] Recorder Handler Initiated, Client ID', host, clientId, 'App ID', appId, performance.now());
-        ;
 
         (window as any).ARC = {};
         (window as any).ARC.stop = () => {
             if((window as any).__rec__) {
                 (window as any).__rec__.stop() 
             }
+            if((window as any).__ARC_DEV__) console.log('[ARC] Stopping Recorder');
             localStorage.removeItem('arcsid');
             (window as any).__rec__ = null;
             delete (window as any).__rec__;
@@ -39,6 +41,7 @@ const startARC=(clientId:String, appId:String, src:any, arccsrc=false)=> {
 
         (window as any).ARC.start = () => {
             if(!(window as any).__rec__) {
+                if((window as any).__ARC_DEV__) console.log('[ARC] Starting Recorder');
                 (window as any).__rec__ = new RecorderHandler({clientId, appId, arccsrc});
             } else {
                 console.error('[ARC] Cannot Initilize multiple ARC instances, stop current instance by using window.ARC.stop() method');
